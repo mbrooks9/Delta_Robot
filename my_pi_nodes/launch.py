ecfb809c153667963c7b51647fc549784dcd63c3
mbrooks9 <mdbrooks999@gmail.com>
2026-09-16 15:03:26 -0600

    Create launch.py

---
+++ my_pi_nodes/launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  return LaunchDescription([
+      Node(
+          package='my_pi_nodes',
+          executable='serial_bridge',
+          name='serial_bridge.py',
+          output='screen',
+      ),
+      Node(
+          package='my_pi_nodes',
+          executable='controller',
+          name='Controller',
+          output='screen',
+      ),
+      Node(
+          package='my_pi_nodes',
+          executable='Motor_Cmd',
+          name='DeltaControl',
+          output='screen',
+      ),
+  ])
